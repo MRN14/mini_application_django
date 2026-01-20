0457d844994ea9f7d100a95911c0459ee7f363f5
MRN14 <marinelecam14@gmail.com>
2026-01-20 11:19:22 +0100

    stash table manufacturer

--- mini_application_django/magasin/models.py
+++ mini_application_django/magasin/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Manufacturer(models.Model):
-    name = models.CharField()
+# class Manufacturer(models.Model):
+#     name = models.CharField()
 
 class Products(models.Model):
     ref = models.CharField(max_length=48, primary_key=True),
     name = models.CharField(),
     stock = models.PositiveBigIntegerField(default=0)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
+    # manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
 
