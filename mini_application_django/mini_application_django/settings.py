2336d7068017cbf8ee6f83c35ec4ec7bc847fc2e
MRN14 <marinelecam14@gmail.com>
2026-01-21 10:24:12 +0100

    fix secret key security

--- mini_application_django/mini_application_django/settings.py
+++ mini_application_django/mini_application_django/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tid1ux0jl^j8w%s0c!r1kvl&c44)3u39b^&rzo6_(+eec@mv8m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
