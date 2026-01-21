5d63cf23b9e5a7c7b52a58d5b1756208bdb7a38d
MRN14 <marinelecam14@gmail.com>
2026-01-21 13:57:14 +0100

    extend database

--- mini_application_django/magasin/models.py
+++ mini_application_django/magasin/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-# class Manufacturer(models.Model):
-#     name = models.CharField()
+class Manufacturer(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return f'{ self.name }'
 
 class Product(models.Model):
     ref = models.CharField(max_length=48, primary_key=True)
     name = models.CharField(max_length=48)
     stock = models.PositiveBigIntegerField(default=0)
-    # manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
 
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, null=True )
+    
+
+    # amélioration possible : création de catégories
+    # class Category(models.TextChoices):
+    #     ELECTRONIC = "electronic"
+
+    def __str__(self):
+        return f'{ self.ref }'
 
