dab3fd92c0c363d01a94e598751c72a65cd6daa3
MRN14 <marinelecam14@gmail.com>
2026-01-21 13:57:48 +0100

    set up admin pannel

--- mini_application_django/magasin/admin.py
+++ mini_application_django/magasin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from magasin.models import Product
+from magasin.models import Manufacturer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('ref', 'name', 'stock')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Manufacturer)
